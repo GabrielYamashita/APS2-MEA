56b27e2f0954c91c2a3e0a963a400c7cec6298ec
Gabriel Yamashita <gabriel.yamashita01@gmail.com>
2022-10-29 18:04:23 -0300

    Update APS2-MEA.cpp
    Comentários Adicionados

--- APS2-MEA.cpp
+++ APS2-MEA.cpp
@@ -1,21 +1,21 @@
 #include "mbed.h"
 
-DigitalOut enA(D2);
-DigitalOut Ap(D5);
-DigitalOut Am(D4);
+DigitalOut enA(D2); // Enable A
+DigitalOut Ap(D5);  // A+
+DigitalOut Am(D4);  // A-
 
-DigitalOut enB(A4);
-DigitalOut Bp(A0);
-DigitalOut Bm(A1);
+DigitalOut enB(A4); // Enable B
+DigitalOut Bp(A0);  // A+
+DigitalOut Bm(A1);  // A-
 
-DigitalIn DIRECAO(D7);
-DigitalIn EN(D6);
+DigitalIn DIRECAO(D7); // Botão Direção
+DigitalIn EN(D6);      // Botão Enable
 
-InterruptIn step(D8);
+InterruptIn step(D8);  // Sinal de Entrada do Gerador de Onda
 
-int passos = 0;
+int passos = 0; // Contagem de Passos
 
-void setBobinas(int bobina) {
+void setBobinas(int bobina) { // Função para Atribuir os Bits em cada Variável
     enA = (bobina/100000) % 10;
     Ap  = (bobina/ 10000) % 10;
     Am  = (bobina/  1000) % 10;
@@ -24,12 +24,12 @@ void setBobinas(int bobina) {
     Bm  = (bobina/     1) % 10;
 }
 
-void Step() {
-    if(DIRECAO == 1) {
+void Step() { // Função para Contar os Passos
+    if(DIRECAO == 1) { // Horário
         passos++;
         if(passos >= 4) {passos = 0;}
     }
-    else if(DIRECAO == 0) {
+    else if(DIRECAO == 0) { // Anti-Horário
         passos--;
         if(passos <= -1) {passos = 3;}
     }
@@ -37,13 +37,13 @@ void Step() {
 
 
 int main() {
-    const int valores[4] = {110110, 101110, 101101, 110101};
+    const int valores[4] = {110110, 101110, 101101, 110101}; // Bits de Ativação de Acionamento do Motor para Cada Passo
     
     while(1) {
-        step.rise(&Step);
+        step.rise(&Step); // Cada Borda de Subida, aciona a Função Step()
         
-        while(EN == 1) {
-            setBobinas(valores[passos]);
+        while(EN == 1) { // Habilita o Motor
+            setBobinas(valores[passos]); // Aciona o Motor, a partir da Lista de Bits, dependendo do Passo
         }
     }
 }
